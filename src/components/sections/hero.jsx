770cf086df56e0059bf479b66966fb6c95027d1a
iMac <imac@Peggys-iMac.local>
2024-04-22 14:22:48 -0700

    added link to  register with us button

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Heading from "../text/heading";
 import Paragraph from "../text/paragraph";
-import Button from "../buttons/button";
+import { Link } from "react-router-dom";
 import her from "../../assets/images/her.png";
 
 export default function Hero() {
   return (
-
     <div className=" p-10 pt-36 mb-10 h-full max-md:flex-col max-md:p-5 max-md:pt-40 bg-white flex items-center justify-center w-full ">
       <div className="w-1/2 max-md:w-full max-md:text-center">
-
         <Heading
           text={"FarmConnect - Your Bridge to Fresh, Locally Sourced Produce"}
           styles={"mb-10 max-md:w-full text-6xl leading-[4rem]"}
@@ -17,15 +15,12 @@ export default function Hero() {
         <Paragraph text="At FarmConnect,  we believe that everyone deserves access to fresh, farm-to-table produce. We are here to connect you directly with farmers in your area, ensuring that you receive the highest quality products while supporting local agriculture. " />
 
         <div className="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
-          <input
-            id="email"
-            type="text"
-            className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-300 dark:text-gray-300 dark:focus:border-green-800 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-green-300"
-            placeholder="Sign up to our newsletter"
-          />
-          <button className="w-full px-5 py-2 text-sm tracking-wider text-white  transition-colors duration-300 transform bg-green-800 rounded-lg lg:w-auto lg:mx-4 hover:bg-green-500 focus:outline-none focus:bg-green-500">
-            Join Us
-          </button>
+          <Link
+            to="/signup"
+            className="w-full px-5 py-2 text-sm tracking-wider text-white transition-colors duration-300 transform bg-green-800 rounded-lg lg:w-auto lg:mx-4 hover:bg-green-500 focus:outline-none focus:bg-green-500"
+          >
+            Register With Us
+          </Link>
         </div>
 
         {/* <div className="my-10 max-md:border-none max-md:p-0  flex flex-col gap-3 text-xs   w-2/3 max-sm:w-full  p-3 border-black border-[1px] max-md:items-center rounded-md">
@@ -39,12 +34,7 @@ export default function Hero() {
         </div> */}
       </div>
 
-      <img
-        src={her}
-        alt=""
-        className="h-full w-1/2 max-md:h-[] max-md:w-full object-contain"
-      />
-
+      <img src={her} alt=""  className="h-full w-1/2 max-md:h-[] max-md:w-full object-contain" />
     </div>
   );
 }
