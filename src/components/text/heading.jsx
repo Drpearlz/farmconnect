646bde96f620feffdd5554df534a03aab9ebec1c
Jeffrey Omamoke <101363689+Neon-jeff@users.noreply.github.com>
2024-04-22 14:38:40 +0100

    filter product page

--- src/components/text/heading.jsx
+++ src/components/text/heading.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Heading({ text, styles }) {
   return (
     <>
-      <h1 className={`  ${styles} font-semibold max-md:text-3xl`}>
-        {text}
-      </h1>
+      <h1 className={` max-md:text-3xl ${styles} font-semibold `}>{text}</h1>
     </>
   );
 }
