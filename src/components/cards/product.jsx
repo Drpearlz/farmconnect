a09101b660a0d441ddd1cc5cc5db5693b7199574
Jeffrey Omamoke <101363689+Neon-jeff@users.noreply.github.com>
2024-04-22 13:08:42 +0100

    product page fix

--- src/components/cards/product.jsx
+++ src/components/cards/product.jsx
@@ -9,7 +9,7 @@ export default function ProductCard({img,price,description,farm}) {
       <img
         src={imgg}
         alt=""
-        class="h-32 w-[300px] max-md:h-28 object-cover rounded-lg"
+        class="h-36 w-[300px] max-md:h-28 object-cover rounded-lg"
       />
       <div class="flex justify-between items-center my-2">
         <Paragraph text="Basket" styles="font-medium max-md:text-xs" />
@@ -20,14 +20,14 @@ export default function ProductCard({img,price,description,farm}) {
       </div>
  
       {/* tags for product goes here */}
-      <div class='grid grid-cols-3 max-md:grid-cols-2 gap-5 my-2'>
+      <div class='grid grid-cols-3 max-md:grid-cols-2 gap-5 my-2 text-xs'>
         <Paragraph
           text="Vegatable"
-          styles=" max-md:text-[.6rem]  p-3 max-md:p-1 text-center bg-green-100 rounded-md   "
+          styles=" max-md:text-[.6rem] text-xs  p-2 max-md:p-1 text-center bg-green-100 rounded-md   "
         />
         <Paragraph
           text="Vegatable"
-          styles=" max-md:text-[.6rem]  p-3 max-md:p-1 text-center bg-green-100 rounded-md   "
+          styles=" max-md:text-[.6rem] text-xs  p-2 max-md:p-1 text-center bg-green-100 rounded-md   "
         />
 
       </div>
