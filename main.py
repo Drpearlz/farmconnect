09121abd703a1969607e6246e9ecafc125d4900f
Jeffrey Omamoke <101363689+Neon-jeff@users.noreply.github.com>
2024-04-26 23:03:57 +0100

    Cors Config

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Union
 from contextlib import asynccontextmanager
 from database.database import create_db
 from routers import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app=FastAPI()
@@ -11,6 +12,14 @@ app=FastAPI()
 create_db()
 
 app.include_router(auth.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_headers=True,
+    allow_methods=True 
+)
 
 if __name__=='__main__':
     uvicorn.run(
